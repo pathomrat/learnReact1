157bcf4058d231ddf732d66706f6bcafce5bb71f
pathomrat97 <apathomrat@gmail.com>
2020-04-24 18:17:02 +0700

    firstCommit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Header from './component/Header';
+import Footer from './component/Footer';
+import ProductItem from './component/product/ProductItem';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <Header />
+      
+      <ProductItem productName="IPhone X" unitPrice="39900" />
+      <ProductItem productName="IPad Pro 2020" unitPrice="29900" />
+      <ProductItem productName="Macbook Air 2020" unitPrice="29900" />
+
+      <Footer company="pathomrat Co." email="pathomrat97@gmail.com" />
     </div>
   );
 }
