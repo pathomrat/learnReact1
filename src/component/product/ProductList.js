068cd1159e3f3459cf5b6776598acaf336d03068
pathomrat97 <apathomrat@gmail.com>
2020-04-28 23:32:20 +0700

    Update ProductList

--- src/component/product/ProductList.js
+++ src/component/product/ProductList.js
@@ -4,12 +4,13 @@ import ProductItem from './ProductItem';
 class ProductList extends Component {
 
     showProducts() {
-        return (this.props.products && this.props.products.map(product => (
+        return (this.props.products && Array.isArray(this.props.products) &&this.props.products.map(product => (
             <ProductItem key={product.id} product={product} onAddOrder={this.props.onAddOrder}
             onDelProduct={this.props.onDelProduct} onEditProduct = {this.props.onEditProduct} />
         )
         ))
     }
+
 
     render() {
 
